f6e3743af28af5ea71afe4a7c0d9d7805ec39b4a
WangChen-PC\WangChen <chen_glassyuki@163.com>
2016-09-27 09:12:51 +0800

    等待制作readme

--- out/main.js
+++ out/main.js
@@ -1,46 +1,18 @@
-/*class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}*/
-function pyramid(a) {
-    for (var i = 1; i <= a; i++) {
-        var b = '';
-        for (var j = 0; j <= a - i - 1; j++) {
-            b += ' ';
+function PrintPyramid(numOfPilesAll) {
+    var numOfPilesNow = 0;
+    var space = 0;
+    var paintOfPlies = "";
+    var asterisk = 0;
+    for (numOfPilesNow = 1; numOfPilesNow <= numOfPilesAll; numOfPilesNow++) {
+        paintOfPlies = "";
+        for (space = 0; space <= numOfPilesAll - numOfPilesNow - 1; space++) {
+            paintOfPlies += " ";
         }
-        for (var k = 1; k <= 2 * i - 1; k++) {
-            b += '*';
+        for (asterisk = 1; asterisk <= 2 * numOfPilesNow - 1; asterisk++) {
+            paintOfPlies += "*";
         }
-        console.log(b);
+        console.log(paintOfPlies);
     }
 }
-//var numS:string = prompt("请输入一个数字","");
-//var num:number = +numS;
-//var num:number = parseInt(numS);
-window.onload = function () {
-    //var el = document.getElementById('content');
-    //var greeter = new Greeter(el);
-    //greeter.start();
-    //var numS:string = process.argv[2];
-    //pyramid(parseInt(numS));
-    pyramid(5);
-};
+PrintPyramid(5);
 //# sourceMappingURL=main.js.map
